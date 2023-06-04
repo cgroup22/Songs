5ecedfade17645273176a5118eaa9169428e3b51
NatiYP <131907360+NatiYP@users.noreply.github.com>
2023-06-04 22:40:14 +0200

    w

--- Miraculous-Online-Music-Store-Template/js/OurJS.js
+++ Miraculous-Online-Music-Store-Template/js/OurJS.js
@@ -3,6 +3,7 @@ const apiStart = `https://localhost:${port}/api`;
 $(document).ready(function () {
     $("#RegisterForm").submit(Register);
     $("#LoginForm").submit(Login);
+    TryLogin();
 
 
     // מוחק את המידע מטופס ההרשמה והלוגין אם לחצו אסקייפ לסגירתו
@@ -75,13 +76,16 @@ function LoginSuccessCallback(data) {
     if (KeepSignedIn)
         localStorage['User'] = JSON.stringify(data);
     else sessionStorage['User'] = JSON.stringify(data);
-    // console.log(data)
-    document.getElementById("LoginErrorMSG").innerHTML = "";
+    /*document.getElementById("LoginErrorMSG").innerHTML = "";
+
+    document.getElementById('LoginRegisterAccountHeader').innerHTML = `<a href="upload.html" class="ms_btn">upload</a>`
+    + `<a href="javascript:;" class="ms_admin_name">Hello ${data.name.split(' ')[0]} <span class="ms_pro_name">${GetFirstLettersOfName(data.name)}</span></a><ul class="pro_dropdown_menu"><li><a href="profile.html">Profile</a></li>` + 
+    `<li><a href="manage_acc.html" target="_blank">Pricing Plan</a></li><li><a href="blog.html" target="_blank">Blog</a></li><li><a href="#">Setting</a></li>` +
+    `<li><a href="#">Logout</a></li></ul>`;
+    $('#myModal1').modal('hide');*/
+    location.href = window.location.pathname.split('/').pop();
 }
 function LoginErrorCallback(error) {
-    // TODO: change the alert
-    console.log(error)
-    // alert(error.responseJSON.message)
     document.getElementById("LoginErrorMSG").innerHTML = error.responseJSON.message;
 }
 function RemoveErrorMesseages() {
@@ -93,4 +97,37 @@ function RemoveErrorMesseages() {
     document.getElementById("RegisterName").value = "";
     document.getElementById("LoginEmail").value = "";
     document.getElementById("LoginPassword").value = "";
+}
+// מקבלת שם, ומחזירה את האותיות הראשונות לפי השם הפרטי ושם המשפחה
+// מקסימום - 3 אותיות, זה משומש בתמונה של המשתמש אחרי ההתחברות
+function GetFirstLettersOfName(name) {
+    let s = name.split(' ');
+    let res = '';
+    for (i in s) {
+        if (i > 2)
+            break;
+        res += s[i][0];
+    }
+    return res;
+}
+function IsLoggedIn() {
+    return sessionStorage['User'] != undefined || localStorage['User'] != undefined;
+}
+function TryLogin() {
+    if (!IsLoggedIn()) return; // Returns if the user is not logged in
+    let data = localStorage['User'] == undefined ? JSON.parse(sessionStorage['User']) : JSON.parse(localStorage['User']);
+    /*document.getElementById('LoginRegisterAccountHeader').innerHTML = `<a href="upload.html" class="ms_btn">upload</a>`
+    + `<a href="javascript:;" class="ms_admin_name">Hello ${data.name.split(' ')[0]} <span class="ms_pro_name">${GetFirstLettersOfName(data.name)}</span></a><ul class="pro_dropdown_menu"><li><a href="profile.html">Profile</a></li>` + 
+    `<li><a href="manage_acc.html" target="_blank">Pricing Plan</a></li><li><a href="blog.html" target="_blank">Blog</a></li><li><a href="#">Setting</a></li>` +
+    `<li><a href="#">Logout</a></li></ul>`;*/
+    document.getElementById('LoginRegisterAccountHeader').innerHTML = `<a href="javascript:;" class="ms_admin_name">Hello ${data.name.split(' ')[0]} <span class="ms_pro_name">${GetFirstLettersOfName(data.name)}</span></a><ul class="pro_dropdown_menu"><li><a href="profile.html">Profile</a></li>` + 
+    `<li><a href="manage_acc.html" target="_blank">Pricing Plan</a></li><li><a href="blog.html" target="_blank">Blog</a></li><li><a href="#">Setting</a></li>` +
+    `<li><a href="#">Logout</a></li></ul>`;
+    document.getElementById('NeedsMSProfile').classList.add('ms_profile');
+}
+// Removes the account from the browser storage
+function Logout() {
+    // TODO
+    localStorage['User'] = "";
+    sessionStorage['User'] = "";
 }
