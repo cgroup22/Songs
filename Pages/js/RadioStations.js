1bcf752d95bf070dda6a3c96490758f5cb8c13af
NatiYP <131907360+NatiYP@users.noreply.github.com>
2023-07-25 12:58:40 +0200

    r

--- Pages/js/RadioStations.js
+++ Pages/js/RadioStations.js
@@ -1,51 +1,51 @@
 // define RadioStations to play live radios.
 RadioStations = [
 {
-    image: "https://images.unsplash.com/photo-1415201364774-f6f0bb35f28f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",	
-    title: "Jazz Radio Live",
+    image: "images/kidsradio.png",	
+    title: "Kids Radio Live",
     artist: "LIVE STATION",
-    mp3: `http://mediaserv38.live-streams.nl:8006/live`,
-    oga: `http://mediaserv38.live-streams.nl:8006/live`,
+    mp3: `https://stream.radiojar.com/gngfpx33hwzuv`,
+    oga: `https://stream.radiojar.com/gngfpx33hwzuv`,
     option : ""
 },
 {
     image: "https://freerangestock.com/sample/64357/pop-music-means-sound-track-and-melodies.jpg",	
+    title: "German Pop Radio Live",
+    artist: "LIVE STATION",
+    mp3: `https://streams.90s90s.de/dab-national/mp3-192/`,
+    oga: `https://streams.90s90s.de/dab-national/mp3-192/`,
+    option : ""
+},
+{
+    image: "https://static.mytuner.mobi/media/tvos_radios/q8ne5lhjxbcf.jpg",	
+    title: "German Dance Radio Live",
+    artist: "LIVE STATION",
+    mp3: `https://streams.90s90s.de/danceradio/mp3-192/`,
+    oga: `https://streams.90s90s.de/danceradio/mp3-192/`,
+    option : ""
+},
+{
+    image: "https://static.mytuner.mobi/media/tvos_radios/mmvGSBqcQB.png",	
+    title: "Dance Radio Live",
+    artist: "LIVE STATION",
+    mp3: `https://streams.90s90s.de/danceradio/mp3-192/`,
+    oga: `https://streams.90s90s.de/danceradio/mp3-192/`,
+    option : ""
+},
+{
+    image: "https://www.radio.net/images/broadcasts/81/9d/104930/2/c300.png",	
+    title: "German Kids Radio Live",
+    artist: "LIVE STATION",
+    mp3: `https://stream.radiojar.com/gngfpx33hwzuv`,
+    oga: `https://stream.radiojar.com/gngfpx33hwzuv`,
+    option : ""
+},
+{
+    image: "images/popr.png",
     title: "Pop Radio Live",
     artist: "LIVE STATION",
-    mp3: `http://mediaserv30.live-streams.nl:8086/live`,
-    oga: `http://mediaserv30.live-streams.nl:8086/live`,
-    option : ""
-},
-{
-    image: "https://images.unsplash.com/photo-1526142684086-7ebd69df27a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",	
-    title: "Classical Radio Live",
-    artist: "LIVE STATION",
-    mp3: `http://mediaserv30.live-streams.nl:8088/live`,
-    oga: `http://mediaserv30.live-streams.nl:8088/live`,
-    option : ""
-},
-{
-    image: "https://images.unsplash.com/photo-1531266752426-aad472b7bbf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",	
-    title: "World Radio Live",
-    artist: "LIVE STATION",
-    mp3: `http://mediaserv38.live-streams.nl:8027/live`,
-    oga: `http://mediaserv38.live-streams.nl:8027/live`,
-    option : ""
-},
-{
-    image: "https://static.vecteezy.com/system/resources/previews/001/263/630/non_2x/clef-music-symbol-vector.jpg",	
-    title: "Gold Radio Live",
-    artist: "LIVE STATION",
-    mp3: `http://mediaserv30.live-streams.nl:8000/live`,
-    oga: `http://mediaserv30.live-streams.nl:8000/live`,
-    option : ""
-},
-{
-    image: "https://www.libertyparkmusic.com/wp-content/uploads/Depositphotos_74762549_xl-2015.jpg",	
-    title: "Latin Radio Live",
-    artist: "LIVE STATION",
-    mp3: `http://mediaserv33.live-streams.nl:8034/live`,
-    oga: `http://mediaserv33.live-streams.nl:8034/live`,
+    mp3: `https://streams.90s90s.de/dab-national/mp3-192/`,
+    oga: `https://streams.90s90s.de/dab-national/mp3-192/`,
     option : ""
 }
 ];
